261f62f89fec09666cdd7066a204a025615c6f00
古人云 <42212645+Hunterang@users.noreply.github.com>
2021-09-16 15:42:26 +0800

    Update event.js

--- vue源码分析/event.js
+++ vue源码分析/event.js
@@ -1,9 +1,9 @@
 export function initEvents(vm) {
     vm._events = Object.create(null)
-    vm._hasHookEvent = false
+    vm._hasHookEvent = false //初始化的时候证明这个是没有的
     const listeners = vm.$options._parentListeners
-    if (listeners) {
-        updateComponentListeners(vm, listeners)
+    if (listeners) { //证明传递的是个component   || 此处不予深入
+        updateComponentListeners(vm, listeners) 
     }
 }
 
@@ -27,5 +27,55 @@ export function eventsMixin(Vue) {
                // 例子 this.$on('hook:smeil')
             }
         }
+        return vm
     } 
+
+    Vue.prototype.$once = function(event, fn) {
+        const vm = this
+
+        function on () {
+            vm.$off(event, fn)
+            fn.call(vm, arguments)
+        }    
+
+        // vm._events[event] = on
+        on.fn = fn
+        vm.$on(event, on)
+        return vm
+    }
+
+    Vue.prototype.$off = function(event, fn) { 
+        const vm = this
+
+        if(!arguments.length) {
+            vm._events = Object.create(null)
+            return vm
+        }
+
+        if(Array.isArray(event)) {
+            for(let i = 0, l = event.length; i < l; i++) {
+                let e = event[i]
+                delete vm._events[e]  //简化处理全部删除
+            }
+            return vm
+        }
+
+        const cbs = vm._events[event]
+        if (!cbs) {
+           return vm
+        }
+        if (!fn) {
+            vm._events[event] = null
+            return vm
+        }
+    }
+
+    Vue.prototype.$emit = function (event,...args) {
+        const vm = this
+        let cbs = vm._events[event]
+        for (let i = 0, l = cbs.length; i < l; i++) {
+            cbs[i].call(vm,...args)
+         }
+
+    }
 }
